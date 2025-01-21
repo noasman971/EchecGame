3c944780716393bb4d743555477d9c23d3170ded
noasman971 <noah.kichenassamy@edu.esiee-it.fr>
2025-01-21 10:08:19 +0100

    Place the bomb

--- src/Destroy.java
+++ src/Destroy.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Destroy {
+
+    /**
+     * Ask to the player what coords to Destroy
+     * @return coord the coords chose by the player
+     */
     public static int[] AskToDestroy() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose an emplacement to Destroy \uD83D\uDCA3\uD83D\uDCA5");
@@ -15,20 +20,35 @@ public class Destroy {
         return coord;
 
     }
-    public static void PlaceBomb(byte[][] grid, int[] coord) {
-        if (grid[coord[0]][coord[1]] ==0 ) {
-            grid[coord[0]][coord[1]] = 6;
+    /**
+     * Place the Bomb to the coords
+     * @param grid the grid of the game
+     * @param coord the coord chose by the Player
+     */
+    public static void PlaceTheBomb(byte[][] grid, int[] coord) {
+        boolean canIPlace = false;
+        while (!canIPlace) {
+            if(coord[0]<11 && coord[1]<11) {
+                if (grid[coord[0]][coord[1]] == 0) {
+                    grid[coord[0]][coord[1]] = 6;
+                    canIPlace = true;
+                }
+                else {
+                    System.out.println("You can't destroy this position");
+                    coord = AskToDestroy();
+                }
+            }
+            else {
+                System.out.println("You can't destroy this position");
+                coord = AskToDestroy();
+            }
         }
-        else{
-            System.out.println("Mauvais choix");
-        }
-
-
     }
+
 
     public static void main(String[] args) {
         Game.main(args);
-        PlaceBomb(Game.grid, AskToDestroy());
+        PlaceTheBomb(Game.grid, AskToDestroy());
         Game.see_grid(Game.grid);
 
 
